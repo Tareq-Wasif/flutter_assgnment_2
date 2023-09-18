a22ed94f1911a97c9b5aada23d35c801ce587b08
Tareq Hossen <135951458+Tareq-Wasif@users.noreply.github.com>
2023-09-18 12:31:11 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,29 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          primarySwatch: Colors.blue
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-  static const List<String> imageLink = [
+  const MyHomePage({Key? key});
+
+  static const List<String> imageLinks = [
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZYDmYNyRzoZheVVmDPUWynWSVrjmftv8wfg&usqp=CAU',
     'https://images.statusfacebook.com/profile_pictures/cute_cats/cute_cats_profile_pictures_04.jpg',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNVQwRMBEt8NiIHofi0isZlVVzWSE8RAF6wD5Zrq_gTFTMhDrSWUUN_01O0uRfFrnSg_Q&usqp=CAU',
@@ -30,11 +31,12 @@ class MyHomePage extends StatelessWidget {
     'https://clipart-library.com/2023/cute-doe-eyes-cat-profile-picture-sd8x26toa0mruwvc.jpg',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLuyc3n7pRSDiD3RP4TuS5zHBDAlNtReh13A6Sn_FvUZYRngiY5cDryoKLuUwJGoC6T4Y&usqp=CAU'
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Photo Gallery'),
+        title: Text('Photo Gallery'),
         centerTitle: true,
       ),
       body: SingleChildScrollView(
@@ -57,59 +59,9 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: GridView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: imageLink.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    mainAxisSpacing: 25,
-                    crossAxisSpacing: 30,
-                  ),
-                  itemBuilder: (context, index) {
-                    return GridTile(
-                      footer: Container(color: Colors.white,child: Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: Center(child: Text("Photo $index",style: const TextStyle(fontSize: 20),)),
-                      ),),
-                      child: GestureDetector(
-                        onTap: () {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('Photo $index clicked!'),
-                            ),
-                          );
-                        },
-                        child: Container(
-                          height: 150,
-                          color: Colors.grey,
-                          child: Image.network(
-                            imageLink[index],
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-            SizedBox(
-              child: ListView.builder(
-                itemCount: 3,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder:  (context, iindex) =>  ListTile(
-                  leading: CircleAvatar(backgroundImage:NetworkImage(imageLink[iindex]),),
-                  title: Text('Photo ${iindex+1}'),
-                  subtitle: Text('Description of photo ${iindex+1}'),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10,),
+            _buildGrid(),
+            _buildListView(),
+            const SizedBox(height: 10),
             FloatingActionButton(
               onPressed: () {
                 ScaffoldMessenger.of(context).showSnackBar(
@@ -125,5 +77,72 @@ class MyHomePage extends StatelessWidget {
       ),
     );
   }
-}
 
+  Widget _buildGrid() {
+    return SizedBox(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: imageLinks.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 25,
+            crossAxisSpacing: 30,
+          ),
+          itemBuilder: (context, index) {
+            return GridTile(
+              footer: Container(
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Center(
+                    child: Text(
+                      'Photo $index',
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Photo $index clicked!'),
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 150,
+                  color: Colors.grey,
+                  child: Image.network(
+                    imageLinks[index],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildListView() {
+    return SizedBox(
+      child: ListView.builder(
+        itemCount: 3,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) => ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(imageLinks[index]),
+          ),
+          title: Text('Photo ${index + 1}'),
+          subtitle: Text('Description of photo ${index + 1}'),
+        ),
+      ),
+    );
+  }
+}
